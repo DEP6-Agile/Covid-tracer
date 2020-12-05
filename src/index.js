aee0c9e0607ef46a1387e0a7858131599ca9a1c4
Damika Anupama <damikaanupama@gmail.com>
2020-12-05 17:33:36 +0530

    add input_form.js and modify input_form.html,index.js,style.scss

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ $("#container").append(customer_log,input_form,login_form,reports_form,signup_fo
 
 $(".content-wrapper").addClass("d-none");
 $("#input_form").removeClass("d-none");
-
 // import '../node_modules/admin-lte/plugins/jquery/jquery.min.js';
 // import '../node_modules/admin-lte/plugins/jquery-ui/jquery-ui.min.js';
 // <!-- Bootstrap 4 -->
 import '../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
 import '../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js';
 import '../node_modules/admin-lte/dist/js/adminlte.js';
+import '../node_modules/datatables.net/js/jquery.dataTables.min.js';
 import './js/menu';
 
 import './scss/style.scss';
