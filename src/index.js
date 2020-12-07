e4b87dc35241b5c8986bda7c48ae2f22961545be
Sathimantha Malalasekera <sathimantha@gmail.com>
2020-12-07 10:09:38 +0530

    Add digital clock and calender

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import input_form from './html_parts/input_form.html';
 import login_form from './html_parts/login_form.html';
 import reports_form from './html_parts/reports.html';
 import signup_form from './html_parts/signup.html';
-var cusArray=[];
-var rowsPerPage=0;
+var cusArray = [];
+var rowsPerPage = 0;
 var numPages;
-var remainRows; 
+var remainRows;
 
 
-$("#container").append(customer_log,input_form,login_form,reports_form,signup_form);
+$("#container").append(customer_log, input_form, login_form, reports_form, signup_form);
 
 $(".content-wrapper").addClass("d-none");
 $("#input_form").removeClass("d-none");
@@ -33,7 +33,7 @@ import './scss/style.scss';
 /* ========================== js for customer_log.html ============ */
 import { getJSON } from 'jquery';
 
-var Customer=function(date,name,id,phone,address,temper,timeIn,timeOut){
+var Customer = function (date, name, id, phone, address, temper, timeIn, timeOut) {
 
     cusArray.push(arguments);
 
@@ -41,91 +41,91 @@ var Customer=function(date,name,id,phone,address,temper,timeIn,timeOut){
 }
 
 
-for(var i=0;i<39;i++){
-Customer('12/02/20','Sajeewa','C00'+i,'0713274293','Kegalle',35.5,'08.30','09.30');
+for (var i = 0; i < 39; i++) {
+    Customer('12/02/20', 'Sajeewa', 'C00' + i, '0713274293', 'Kegalle', 35.5, '08.30', '09.30');
 }
 
-var heightCheck=function(){
-
-    
-      var rowCounter=0;
-      var keepLoop=true;
-      
-while(keepLoop){
+var heightCheck = function () {
 
 
-    var tRow=$('<tr></tr>');
-        var tData=$('<td></td>');
+    var rowCounter = 0;
+    var keepLoop = true;
+
+    while (keepLoop) {
+
+
+        var tRow = $('<tr></tr>');
+        var tData = $('<td></td>');
         tData.text("testing");
         tRow.append(tData);
-    
-      $('#customer-table').append(tRow); 
-      rowCounter=rowCounter+1;
- 
-      if($(window).innerHeight()<$(document).height()){
-        rowsPerPage=rowCounter-1;
-        keepLoop=false;
-      
-  }
-   
-}
+
+        $('#customer-table').append(tRow);
+        rowCounter = rowCounter + 1;
+
+        if ($(window).innerHeight() < $(document).height()) {
+            rowsPerPage = rowCounter - 1;
+            keepLoop = false;
+
+        }
+
+    }
 }
 
 $("#customer-log-link").click(function () {
-  rowsPerPage=0;
-  $('#customer-table tr').remove();
+    rowsPerPage = 0;
+    $('#customer-table tr').remove();
     heightCheck();
     $('#customer-table tr').remove();
-      
-        addRows(0,rowsPerPage);
-        pagination();
-        
+
+    addRows(0, rowsPerPage);
+    pagination();
+
 });
 
 
 console.log(Customer.length);
-var addRows=function(start,end){
-    for(start;start<end+1;start++){
-        var tRow=$('<tr></tr>');
+var addRows = function (start, end) {
+    for (start; start < end + 1; start++) {
+        var tRow = $('<tr></tr>');
 
-    for(var i=0;i<Customer.length;i++){
-        var tData=$('<td></td>');
-        tData.text(cusArray[start][i]);
-        tRow.append(tData);
-    } 
-    $('#customer-table').append(tRow);
-    console.log('table height',tRow.height());
-    
-}
-
-}
-
-
-var pagination=function(){
-         numPages=parseInt((cusArray.length)/rowsPerPage);
-        remainRows=parseInt((cusArray.length)%rowsPerPage);
-        console.log(numPages,remainRows);
-
-        for(var i=2;i<numPages+1;i++){
-         
-           insPageLink(i);
-          
+        for (var i = 0; i < Customer.length; i++) {
+            var tData = $('<td></td>');
+            tData.text(cusArray[start][i]);
+            tRow.append(tData);
         }
-        if(remainRows>0){
-          insPageLink(numPages+1);
+        $('#customer-table').append(tRow);
+        console.log('table height', tRow.height());
 
-        }
+    }
+
 }
 
-var insPageLink=function(i){
-    var pageItem=$('<li></li>');
-    pageItem.attr('class',"page-item");
-     var pageLink=$('<a></a>');
-     pageLink.attr('class',"page-link");
-     pageLink.attr('href',"#");
-     pageLink.text(i);
-     pageItem.append(pageLink);
-     $('#btn-forward').before(pageItem);
+
+var pagination = function () {
+    numPages = parseInt((cusArray.length) / rowsPerPage);
+    remainRows = parseInt((cusArray.length) % rowsPerPage);
+    console.log(numPages, remainRows);
+
+    for (var i = 2; i < numPages + 1; i++) {
+
+        insPageLink(i);
+
+    }
+    if (remainRows > 0) {
+        insPageLink(numPages + 1);
+
+    }
+}
+
+var insPageLink = function (i) {
+    var pageItem = $('<li></li>');
+    pageItem.attr('class', "page-item");
+    var pageLink = $('<a></a>');
+    pageLink.attr('class', "page-link");
+    pageLink.attr('href', "#");
+    pageLink.text(i);
+    pageItem.append(pageLink);
+    $('#btn-forward').before(pageItem);
 
 }
 
@@ -137,47 +137,88 @@ console.log($('#customer-table').innerHeight());
 
 
 
-$(document).on('click', function(event){
-  
-    
-    var clickedPage=event.target.textContent;
+$(document).on('click', function (event) {
 
-     if((event.target.id=='fas fa-backward')||(event.target.id=='backward')){
-        $('#customer-table tr').remove();
-        addRows(0,rowsPerPage);
-         return;
-     }
 
-     if((event.target.id=='fas fa-forkward')||(event.target.id=='forward')){
+    var clickedPage = event.target.textContent;
+
+    if ((event.target.id == 'fas fa-backward') || (event.target.id == 'backward')) {
         $('#customer-table tr').remove();
-        addRows(numPages*rowsPerPage,(numPages*rowsPerPage)+remainRows-1);
-         return;
-     }
-    
-     
-      if((event.target.className=='page-link')) 
-     {
-       
-    
-        var startP=((parseInt(clickedPage)-1)*rowsPerPage);
-     
-        if(clickedPage==numPages+1){
+        addRows(0, rowsPerPage);
+        return;
+    }
+
+    if ((event.target.id == 'fas fa-forkward') || (event.target.id == 'forward')) {
+        $('#customer-table tr').remove();
+        addRows(numPages * rowsPerPage, (numPages * rowsPerPage) + remainRows - 1);
+        return;
+    }
+
+
+    if ((event.target.className == 'page-link')) {
+
+
+        var startP = ((parseInt(clickedPage) - 1) * rowsPerPage);
+
+        if (clickedPage == numPages + 1) {
             $('#customer-table tr').remove();
-            addRows(startP,startP+remainRows-1);
-        }else if(clickedPage<numPages+1){
+            addRows(startP, startP + remainRows - 1);
+        } else if (clickedPage < numPages + 1) {
             $('#customer-table tr').remove();
-            
-        addRows(startP,startP+rowsPerPage);
-     
-        }else{
-            
+
+            addRows(startP, startP + rowsPerPage);
+
+        } else {
+
         }
 
-     }
-   
- });
+    }
 
- /* ========================== js for customer_log.html ============ */
+});
+
+/* ========================== js for customer_log.html ============ */
 
 
-  /* ========================== js for input_form.html ============ */
+/* ========================== js for input_form.html ============ */
+/* ========================== js for input_form.html ============ */
+
+
+/* ========================== clock+calender ============ */
+$(document).ready(function () {
+    clockUpdate();
+    setInterval(clockUpdate, 1000);
+})
+
+function clockUpdate() {
+    var date = new Date();
+    function addZero(x) {
+        if (x < 10) {
+            return x = '0' + x;
+        } else {
+            return x;
+        }
+    }
+
+    function twelveHour(x) {
+        if (x > 12) {
+            return x = x - 12;
+        } else if (x == 0) {
+            return x = 12;
+        } else {
+            return x;
+        }
+    }
+
+    var h = addZero(twelveHour(date.getHours()));
+    var m = addZero(date.getMinutes());
+    var s = addZero(date.getSeconds());
+    var dd = addZero(date.getDate());
+    var mm = addZero(date.getMonth() + 1);
+    var yyyy = date.getFullYear();
+
+    $('.digital-clock').text(h + ':' + m + ':' + s);
+    $('.digital-calender').text(dd + '-' + mm + '-' + yyyy);
+    $('.digital-clock').css({ 'color': '#bbbbbb', 'text-shadow': '0 0 6px #ff0' });
+    $('.digital-calender').css({ 'color': '#bbbbbb', 'text-shadow': '0 0 6px #ff0' });
+}
+  /* ========================== clock+calender ============ */
